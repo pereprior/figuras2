cf0a89f647c96fd6fce03cef0ce9904dccbd6b2a
ppe <alupri5332@ieselcaminas.org>
2022-11-10 15:20:10 +0100

    v1

---
+++ rectangulo.java
@@ -0,0 +1,14 @@
+public class rectangulo {
+    private double lado1;
+    private double lado2;
+
+    public rectangulo (double lado1, double lado2){
+        lado1=7;
+        lado2=11;
+    }
+
+    public double obtenerArea(){
+        return lado1*lado2;
+    }
+
+}
